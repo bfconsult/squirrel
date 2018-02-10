10a566937fe4f7500c219eeaf5aa1dd3791ffd2b
James Billson <james@billson.com>
2018-02-10 18:24:40 +1100

    mail

--- protected/views/layouts/mail.php
+++ protected/views/layouts/mail.php
@@ -19,19 +19,19 @@
   		</tr>
   		<tr>
         <td style="color:#424242;font-size:16px;padding-top:5px;">
-            <strong>Reqfire</strong><br />
+            <strong>Squirrel</strong><br />
             <br />
-            The common language for your project: define together, build better.<br />
+            Remember where you put the nuts.<br />
             <br />
             <small><strong>T. +61 (3) 9014 9540</strong></small><br />
-            <small><strong>E. hello@reqFire.com</strong></small><br />
+            <small><strong>E. info@billson.com</strong></small><br />
              <br /> 
-             <a href="https://www.reqFire.com/"><img src="https://www.reqFire.com/app/images/furniture/logo.png" alt="www.reqfire.com" border="0"></a><br />
+             <a href="https://squirrel.billson.com/"><img src="https://squirrel.billson.com/images/logo.png" alt="squirrel.billson.com" border="0"></a><br />
       	</td>
   		</tr>
                   <tr>
                       <td style="line-height:1em; color: #A0A0A0">
-                        <small>The information in this Mail is confidential and may be legally privileged. It is intended solely for the addressee. Access to this Mail by anyone else is unauthorised. If you are not the intended recipient, any disclosure, copying, distribution or any action taken omitted to be taken in reliance of it is prohibited and may be unlawful. Please notify us if you have received this message in error, and remove both emails from your system. Reqfire Pty Ltd's liability in connection with transmitting, unauthorised access to, or viruses in this message and its attachments, is limited to re-supplying this message and its attachments. Thanks.</small>
+                        <small>The information in this Mail is confidential and may be legally privileged. It is intended solely for the addressee. Access to this Mail by anyone else is unauthorised. If you are not the intended recipient, any disclosure, copying, distribution or any action taken omitted to be taken in reliance of it is prohibited and may be unlawful. Please notify us if you have received this message in error, and remove both emails from your system. Billson Fisher Consulting Pty Ltd's liability in connection with transmitting, unauthorised access to, or viruses in this message and its attachments, is limited to re-supplying this message and its attachments. Thanks.</small>
                       </td>
                   </tr>
   	</tbody>
