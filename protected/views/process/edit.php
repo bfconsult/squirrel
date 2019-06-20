10c09ff1b2ccabbac534b99d06634de758c193e1
James Billson <james@billson.com>
2019-06-20 14:29:00 +1000

    edit pages

---
+++ protected/views/process/edit.php
@@ -0,0 +1,18 @@
+<?php
+
+$project=Project::model()->findbyPK(Yii::App()->session['project']);
+
+echo '<h1>'.$project->name;
+    ?>
+    <a href = "/project/view"><i class="icon-arrow-left"></i></a></h1>
+
+
+<h2>Edit Process</h2>
+
+
+
+<?php 
+
+echo $this->renderPartial('_form', array('model'=>$model,'project'=>$project)); ?>
+
+
