1462e6ba7615168743713d401ffdc1a26991d179
James Billson <james@billson.com>
2019-07-10 10:55:40 +1000

    report

---
+++ protected/views/project/report.php
@@ -0,0 +1,159 @@
+<?php
+
+//echo Yii::App()->session['project']; die;
+
+$project=Project::model()->findbyPK(Yii::App()->session['project']);
+$times = Time::model()->getAllProjectTime($project->id);
+
+echo '<h1>'.$project->name;
+
+if (Yii::App()->session['projectOwner']==1) {
+    ?>
+
+    <a href="/project/edit"><i class="icon-edit"></i></a>
+    <a href="/project/delete" onclick="return confirm('Are you sure you want to delete this project?');"><i class="icon-remove-sign"></i></a>
+    <a href="/time/viewdetail"><i class="icon-calendar"></i></a>
+    <?php
+}
+    ?>
+
+
+    </h1>
+<?php
+
+
+
+
+
+
+$startMonth = date("m",$startTimestamp);
+$startDay =date("d",$startTimestamp);
+$endMonth = date("m",$endTimestamp);
+$endDay = date("m",$endTimestamp);
+
+?>
+<div class="row">
+
+<div class="span1">
+From: 
+</div>
+    <div class="span2">
+        <form method="POST" action="">
+              
+                <select  class="input-mini" name="Date[start_month]">
+                <?php for($m=1; $m <13 ; $m++) { ?>
+                <option value="<?php echo $m; ?>"  <?php if ($m == $startMonth) echo 'selected'; ?>  >
+                <?php echo date("M", mktime(0, 0, 0, $m, 10));?>
+                </option>
+                <?php }  ?>
+                </select>
+   
+                <select  class="input-mini" name="Date[start_day]">
+                <?php for($m=1; $m <31 ; $m++) { ?>
+                <option value="<?php echo $m; ?>"  <?php if ($m == $startDay) echo 'selected'; ?>  >
+                <?php echo $m;?>
+                </option>
+                <?php }  ?>
+                </select>
+                
+        </div>
+    <div class="span1">
+To:
+</div>
+<div class="span2">
+            <select  class="input-mini" name="Date[end_month]">
+            <?php for($m=1; $m <13 ; $m++) { ?>
+            <option value="<?php echo $m; ?>" <?php if ($m == $endMonth) echo 'selected'; ?> >
+            <?php echo date("M", mktime(0, 0, 0, $m, 10));?>
+            </option>
+            <?php }  ?>
+            </select>
+            
+            <select  class="input-mini" name="Date[end_day]">
+            <?php for($m=1; $m <31 ; $m++) { ?>
+            <option value="<?php echo $m; ?>" <?php if ($m == $endDay) echo 'selected'; ?> >
+            <?php echo $m;?>
+            </option>
+            <?php }  ?>
+            </select>
+    </div>
+    <div class="span2">
+            <input type="submit" class="btn-primary">
+            </form>
+    </div>
+
+    <div class="span2 pull-right">
+    <form method="POST" action="/time/download">
+
+            <input type="hidden" name="startDate" value="<?php echo $startTimestamp; ?>">
+            <input type="hidden" name="endDate" value="<?php echo $endTimestamp; ?>">
+        
+            <input type="submit" value = "download" class="btn-success">
+
+            </form>
+    </div>
+</div>
+<?php
+
+
+
+    $box = $this->beginWidget('bootstrap.widgets.TbBox', array(
+        'title' => 'Recent Configuration Changes',
+        'headerIcon' => 'icon-briefcase',
+        // when displaying a table, if we include bootstra-widget-table class
+        // the table will be 0-padding to the box
+        'htmlOptions' => array('class' => 'bootstrap-widget-table'),
+        'headerButtons' => array()
+
+    ));
+
+if (!empty($data)) {
+
+    ?>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Name</th>
+
+
+        </tr>
+        </thead>
+        <tbody>
+        <?php if (count($data)): ?>
+
+
+            <?php foreach ($data as $item): ?>
+
+
+                <tr class="odd">
+                    <td>
+                        <?php echo $item->id; ?>
+                        <?php echo $item->name; ?>
+                        <?php echo $item->description; ?>
+                    </td>
+       
+                 
+                    <td>
+                        <?php echo $item->creator->firstname . ' ' . $item->creator->lastname . ' ' . $item->create_date; ?>
+                    </td>
+
+                </tr>
+                <?php
+
+            endforeach;
+        endif;
+
+        ?>
+
+        </tbody>
+    </table>
+
+    <?php
+}
+
+
+
+
+$this->endWidget();
+
+?>
