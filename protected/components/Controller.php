c0a84f2fb76f97fcf2d82b049f599c9e032c92dc
James Billson <james@billson.com>
2019-08-09 17:26:11 +1000

    oeuoaeu

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -8,25 +8,6 @@ class Controller extends CController
 	/**
 	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
 	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
-	 */
-	public $layout='//layouts/column1';
-	/**
-	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
-	 */
-	public $menu=array();
-	/**
-	 * @var array the breadcrumbs of the current page. The value of this property will
-	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
-	 * for more details on how to specify this property.
-	 */
-	public $breadcrumbs=array();
-
-    public $allHelps = null;
-    public $doNotShowHelpsAgain = [];
-    
-    /**
-     * Class constructor
-     *
      */
     
     protected function beforeAction($action)
@@ -66,17 +47,7 @@ class Controller extends CController
 
         }
 
-        try{
-            if((!Yii::app()->user->isGuest) && isset(Yii::app()->user->company->id)){
-                $this->allHelps = Help::model()->findAll(array('order' => 'indexing'));
-                $allHelpsBlocked = User::model()->MetaLoad('help_block');
-                if($allHelpsBlocked && array_key_exists(0, $allHelpsBlocked)){
-                    $this->doNotShowHelpsAgain = $allHelpsBlocked;
-                }
-            }
-        }catch (Exception $e){
-            $this->allHelps = [];
-        }
+
 
         return parent::beforeAction($action);
 	}
