ccb1bd827fd86183f5de479c59bac4b28a2e14da
James Billson <james@billson.com>
2019-08-20 09:49:53 +1000

    fluffing

--- protected/views/project/view.php
+++ protected/views/project/view.php
@@ -202,14 +202,14 @@ echo 'last run value is '.$lastrun;
                     
                     
                     } 
-                       ELSE //IF (($item->frequency-$interval)/2 > $lastrun ) 
+                       ELSEIF (($item->frequency-$interval) > $interval/2 ) 
                         {
                          echo   '<i class="icon-time text-success"></i> due in ';
                          echo floor(($item->frequency-$interval)/(24*60*60)).' days'; 
-                         /*
+                         
                         } ELSE {
                             echo '<i class="icon-ok text-success"></i> good';
-                        */
+                        
                         }
                        
                     }
